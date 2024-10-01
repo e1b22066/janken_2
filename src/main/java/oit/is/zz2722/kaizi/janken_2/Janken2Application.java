fffbf26356ecd4a5113a41a620e6a0a2e9f2d353
e1b22066 <e1b22066@oit.ac.jp>
2024-10-01 14:47:51 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/zz2722/kaizi/janken_2/Janken2Application.java
@@ -0,0 +1,13 @@
+package oit.is.zz2722.kaizi.janken_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Janken2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Janken2Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/zz2722/kaizi/janken_2/Janken2ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.zz2722.kaizi.janken_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Janken2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
